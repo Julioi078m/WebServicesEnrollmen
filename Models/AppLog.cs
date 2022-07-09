e247dd57753dd9dc9aaa1deb03f5e9bf63c7823f
Julio Juarez <jjuarez-i078m@kinal.edu.gt>
2022-07-09 01:38:00 -0600

    Inicio de proyecto git, carga de todos los archivos

---
+++ Models/AppLog.cs
@@ -0,0 +1,27 @@
+using System.Net;
+
+namespace WebServicesEnrollment.Models
+{
+    public class AppLog
+    {
+        public AppLog()
+        { 
+            IPAddress[] address = Dns.GetHostEntry(HostName).AddressList;
+            for (int i = 0; i < address.Length; i++)
+            { 
+                this.Ip = address[i].ToString();
+            }
+        }
+        public string Name { get; set; } = "WebServicesEnrollment";
+        public string HostName { get; set; } = Dns.GetHostName();
+        public string ApiKey { get; set; } = "N/A";
+        public string Uri { get; set; } = "/EnrollmentService.asmx"; 
+        public int ResponseCode { get; set; }
+        public long ResponseTime { get; set; }
+        public string Ip { get; set; }
+        public int Level { get; set; }
+        public string Message { get; set; }
+        public string DateTime { get; set; }
+        public string version { get; set; } = "V1";
+    }
+}
